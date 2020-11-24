9ed1bc6b194bdbcd7b4d1827c1094b8a89713540
roevevgen <roevevgen7@gmail.com>
2020-11-24 21:23:37 +0200

    nav

--- wordpress/wp-content/themes/sportisland/functions.php
+++ wordpress/wp-content/themes/sportisland/functions.php
@@ -5,27 +5,16 @@ add_action('wp_enqueue_scripts', 'si_scripts');
 
 
 function si_setup(){
+    register_nav_menu('menu-header', 'Меню в шапке');
+    register_nav_menu('menu-footer', 'Меню в подвале');
+
     add_theme_support('custom-logo');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+//    add_theme_support('menus');
 }
 
-//function si_scripts(){
-//    wp_enqueue_script(
-//        'js',
-//        get_template_directory_uri() . '/assets/js/js.js',
-//        array(),
-//        '1.0',
-//        true);
-//
-//    wp_enqueue_style(
-//        'si-style',
-//        get_template_directory_uri() . '/assets/css/styles.css',
-//        array(),
-//        '1.0',
-//        'all'
-//    );
-//}
+
 function si_scripts(){
     wp_enqueue_script('js',
         _si_assets_path('js/js.js'),
