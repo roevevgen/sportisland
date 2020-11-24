089b656874dab13f5906e4fa5e61960f62fb9321
roevevgen <roevevgen7@gmail.com>
2020-11-24 23:04:44 +0200

    nav_footer

--- wordpress/wp-content/themes/sportisland/footer.php
+++ wordpress/wp-content/themes/sportisland/footer.php
@@ -32,24 +32,30 @@
                 <?php the_custom_logo();?>
                 <span class="slogan">Твой фитнес клуб всегда рядом!</span>
             </p>
-
+                <?php
+                $locations = get_nav_menu_locations();
+                $menu_id = $locations['menu-footer'];
+                $menu_items = wp_get_nav_menu_items($menu_id,[
+                    'order' => 'ASC',
+                    'orderby' => 'menu_order'
+                ]);
+                ?>
             <nav class="main-navigation">
                 <ul class="main-navigation__list">
-                    <li>
-                        <a href="services.html">Услуги</a>
+                    <?php
+                    $http = 'http' . ( ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ) ? 's' : '' ) . '://';
+                    $url = $http . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+                    foreach ($menu_items as $item):
+                        $class_text = '';
+                    if( $item->url === $url ){
+                        $class_text = 'class="active"';
+                    }
+                        ?>
+                    <li <?php echo $class_text; ?>>
+                        <a href="<?php echo $item->url; ?>">
+                            <?php echo $item->title; ?></a>
                     </li>
-                    <li class="active">
-                        <a href="trainers.html">Тренеры</a>
-                    </li>
-                    <li>
-                        <a href="schedule.html">Расписание</a>
-                    </li>
-                    <li>
-                        <a href="prices.html">Цены</a>
-                    </li>
-                    <li>
-                        <a href="contacts.html">Контакты </a>
-                    </li>
+                    <?php endforeach?>
                 </ul>
             </nav>
             <address class="main-header__widget widget-contacts">
