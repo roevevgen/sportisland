fe1536b6ceb17f3b1bb67050978b0067c9b11476
roevevgen <roevevgen7@gmail.com>
2020-12-03 23:12:59 +0200

    add_meta_box

--- wordpress/wp-content/themes/sportisland/functions.php
+++ wordpress/wp-content/themes/sportisland/functions.php
@@ -16,6 +16,7 @@ add_filter('show_admin_bar', '__return_false');
 add_action('wp_enqueue_scripts', 'si_scripts');
 add_action( 'widgets_init', 'si_register' );
 add_action('init', 'si_register_types');
+add_action( 'add_meta_boxes', 'si_meta_boxes');
 add_shortcode('si-paste-link', 'si_paste_link');
 add_filter('si_widget_text', 'do_shortcode');
 
@@ -303,6 +304,40 @@ function si_register_types()
         'public'                => true,
         'hierarchical'          => true
     ]);
+}
+
+ function si_meta_boxes()
+ {
+     add_meta_box(
+         'si-like',
+         'Кол-во лайков: ',
+         'si_meta_like_cb',
+         'post'
+     );
+     $fields = [
+         'si_order_date' => 'Дата заявки: ',
+         'si_order_name' => 'Имя клиента: ',
+         'si_order_phone' => 'Номер телефона: ',
+         'si_order_choice' => 'Выбор клиента: ',
+     ];
+     foreach ($fields as $slug => $text){
+         add_meta_box(
+             $slug,
+             $text,
+             'si_order_fields_cb',
+             'orders',
+             'advanced',
+             'default',
+             $slug
+         );
+     }
+ }
+
+function si_meta_like_cb( $post_obj ){
+    $likes = get_post_meta( $post_obj->ID, 'si-like', true);
+    $likes = $likes ? $likes : 0;
+//    echo "<input type=\"text\" name=\"si-like\" value=\"${likes}\"";
+    echo '<p>' . $likes . '</p>';
 }
 
 function _si_assets_path($path){
